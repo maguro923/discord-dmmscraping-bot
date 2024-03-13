8b51f6ad379bf3650eaf687686c25cbe26caf605
maguro923 <maguro29y@gmail.com>
2024-03-13 15:31:10 +0900

    first commit

---
+++ DiscordData/DataAccess.py
@@ -0,0 +1,2 @@
+COMMAND = ['btc','eth','shib','ltc']
+command_list = ["btc","eth","xrp","ltc","bch","nidt","algo","ape","avax","axs","bat","chz","enj","etc","fcr","flr","hbar","link","matic","mkr","mona","oas","omg","sand","shib","trx","xlm","zpg"]
